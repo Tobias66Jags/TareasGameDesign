38277575a59c81822ef634d40fc9e6632d3d6e3b
Tobias Gtz <perepump@gmail.com>
2023-10-02 14:33:36 -0600

    light

---
+++ FadedWorldProve/Assets/Master/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed =5f;
+
+
+    private Rigidbody2D _rb2d;
+    private Animator _animator;
+
+    Vector2 _movement;
+
+
+    private void Start()
+    {
+     _rb2d = GetComponent<Rigidbody2D>();
+     _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+
+        _animator.SetFloat("Horizontal", _movement.x);
+        _animator.SetFloat("Vertical", _movement.y);
+        _animator.SetFloat("Speed", _movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        _rb2d.MovePosition(_rb2d.position+ _movement.normalized * _moveSpeed*Time.fixedDeltaTime);
+    }
+}
