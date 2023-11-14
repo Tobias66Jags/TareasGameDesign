859628f8e576618d008817cf52257c73109932df
warcilac9 <warcilac9@gmail.com>
2023-11-14 16:39:37 -0600

    a

--- PPT/Assets/Timer.cs
+++ PPT/Assets/Timer.cs
@@ -5,27 +5,39 @@ using TMPro;
 
 public class Timer : MonoBehaviour
 {
+    public Win win;
+
     public float maxTime = 5;
     public float currentTime;
 
     public TextMeshProUGUI timerView;
 
-
+    public string url1, url2;
     private void Start()
     {
         currentTime = maxTime;
         StartCoroutine(SetTimer());
+      
     }
 
     [ContextMenu("Timer")]
     public void InitializeTimer()
     {
+        
         currentTime = maxTime;
         StartCoroutine(SetTimer());
     }
 
     private void Update()
+
     {
+
+        if (currentTime <= 1)
+        {
+            Debug.Log("UWU");
+            win.winner();
+        }
+      
         Debug.Log(currentTime);
         timerView.text = currentTime.ToString();
     }
@@ -33,6 +45,8 @@ public class Timer : MonoBehaviour
     {
         while (currentTime>0)
         {
+            win.setChooseP1(url1);
+            win.setChooseP2(url2);
             currentTime--;
             yield return new WaitForSecondsRealtime(1);
         }
--- PPT/Assets/Win.cs
+++ PPT/Assets/Win.cs
@@ -1,10 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class Win : MonoBehaviour
 {
  public string P1, P2;
+
+    public void setChooseP1(string p)
+    {
+        StartCoroutine(HacerPeticion(p));
+    }
+    public void setChooseP2(string p)
+    {
+        StartCoroutine(HacerPeticion2(p));
+    }
+
+    private void Update()
+    {
+        
+    }
+    public void winner()
+    {
+        int v = DeterminarGanador(int.Parse(P1), int.Parse(P2));
+        if (v== 0)
+        {
+            Debug.Log("Nadie Ganó");
+        }
+        else if (v == 1)
+        {
+            Debug.Log("Player 1 Ganó");
+        }
+           else if (v == 2)
+        {
+            Debug.Log("Player 2 Ganó");
+        }
+
+    }
 
     public int DeterminarGanador(int decisionJugador1, int decisionJugador2)
     {
@@ -35,6 +67,77 @@ public class Win : MonoBehaviour
         else
         {
             return 0; // Ningún jugador elige una opción válida
+        }
+    }
+
+    public IEnumerator HacerPeticion(string newUrl)
+    {
+        while (true)
+        {
+            using (UnityWebRequest www = UnityWebRequest.Get(newUrl))
+            {
+                yield return www.SendWebRequest();
+
+                if (www.isNetworkError || www.isHttpError)
+                {
+                    Debug.LogError("Error en la solicitud: " + www.error);
+                }
+                else
+                {
+                    // La respuesta de la API está en www.downloadHandler.text
+                    string respuesta = www.downloadHandler.text;
+                   // string respuesta = www.downloadHandler.text;
+                    P1 = respuesta;
+                    // Convierte la respuesta a número (puedes usar int.Parse o float.Parse según el tipo de dato esperado)
+                 
+                        // Imprime el número en la consola
+                        Debug.Log("Número r: " + respuesta);
+
+                    
+                  /*  else
+                    {
+                        Debug.LogError("La respuesta no es un número válido: " + respuesta);
+                    }*/
+                }
+            }
+            yield return new WaitForSecondsRealtime(1f);
+         
+            StopCoroutine(HacerPeticion(null));
+        }
+    } 
+    public IEnumerator HacerPeticion2(string newUrl)
+    {
+        while (true)
+        {
+            using (UnityWebRequest www = UnityWebRequest.Get(newUrl))
+            {
+                yield return www.SendWebRequest();
+
+                if (www.isNetworkError || www.isHttpError)
+                {
+                    Debug.LogError("Error en la solicitud: " + www.error);
+                }
+                else
+                {
+                    // La respuesta de la API está en www.downloadHandler.text
+                    string respuesta = www.downloadHandler.text;
+                   // string respuesta = www.downloadHandler.text;
+                    P2 = respuesta;
+                    // Convierte la respuesta a número (puedes usar int.Parse o float.Parse según el tipo de dato esperado)
+                 
+                        // Imprime el número en la consola
+                        Debug.Log("Número recibido: " + respuesta);
+
+                    
+                  /*  else
+                    {
+                        Debug.LogError("La respuesta no es un número válido: " + respuesta);
+                    }*/
+                }
+            }
+            yield return new WaitForSecondsRealtime(1f);
+          
+            StopCoroutine(HacerPeticion2(null));
         }
     }
 
